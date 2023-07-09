3ad9cdab7c05f72febb49cbf5ab8780ff26ecea6
Марк Савицкий <marksavickij@Marks-MacBook-Pro.local>
2023-07-08 22:51:06 -0700

    done 123

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -10,7 +10,7 @@ const VideoSection = () => {
           <iframe
             width="100%"
             height="300"
-            src="https://www.youtube.com/embed/bbcr06F_PLM"
+            src="https://www.youtube.com/embed/NW4tNiNucms"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
