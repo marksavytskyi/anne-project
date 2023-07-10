b640d5a54786e18fc0e22aec539b1c1e26bea3ee
Марк Савицкий <marksavickij@Marks-MacBook-Pro.local>
2023-07-09 20:41:01 -0700

    doneqdqwd

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,155 +1,25 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
+import { Link } from '@mui/material';
+
+import * as React from 'react';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import styled from 'styled-components';
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
 
-const validationSchema = yup.object({
-  name: yup.string('Enter your name').required('Name is required'),
-  phone: yup
-    .string('Enter your phone number')
-    .matches(/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Enter a valid phone number')
-    .required('Phone number is required'),
-});
+const BasicButtons = () => {
+  const SubmitButton = styled(Button)`
+    width: 100%;
 
-const FormRow = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    align-items: center;
-  }
-`;
-
-const FormItem = styled.div`
-  flex-basis: 100%;
-  margin-bottom: 12px;
-
-  @media (min-width: 768px) {
-    flex-basis: 48%;
-    margin-bottom: 16px;
-  }
-`;
-
-const SubmitButton = styled(Button)`
-  @media (min-width: 768px) {
-    width: 50%;
-    margin-top: 24px;
-  }
-`;
-
-const WhiteTextField = styled(TextField)`
-  .MuiInputBase-input,
-  .MuiInputLabel-root,
-  .MuiOutlinedInput-root {
-    color: white;
-  }
-
-  .MuiOutlinedInput-root {
-    fieldset {
-      border-color: white;
+    @media (min-width: 768px) {
+      width: 300px;
     }
-    &:hover fieldset {
-      border-color: white;
-    }
-    &.Mui-focused fieldset {
-      border-color: white;
-    }
-  }
-`;
+  `;
 
-const WithMaterialUI = () => {
-  const formRef = useRef(null);
-
-  const sendEmail = values => {
-    emailjs
-      .sendForm(
-        'service_m23dptf',
-        'template_8j49fii',
-        formRef.current,
-
-        'oVdIW_p_EG4-iWJxy'
-      )
-      .then(
-        result => {
-          console.log(result.text);
-        },
-        error => {
-          console.log(error.text);
-        }
-      );
-  };
-
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      phone: '',
-    },
-    validationSchema: validationSchema,
-    onSubmit: sendEmail,
-  });
-
-  // const form = formik.form.current;
-
-  const handlePhoneChange = event => {
-    const { value } = event.target;
-    if (!value.startsWith('+')) {
-      event.target.value = '+' + value;
-      formik.handleChange(event);
-    } else {
-      formik.handleChange(event);
-    }
-  };
-
-  // Rest of your component code...
-
+  const formUrl =
+    'https://docs.google.com/forms/d/e/1FAIpQLSfQdVD26DnjU8hahfe762JR5WMG1fUVabjKOG6E_1sahMjs0Q/viewform?usp=sf_link';
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit} ref={formRef}>
-        <FormRow>
-          <FormItem>
-            <WhiteTextField
-              fullWidth
-              id="name"
-              name="name"
-              label="Name"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              error={formik.touched.name && Boolean(formik.errors.name)}
-              helperText={formik.touched.name && formik.errors.name}
-            />
-          </FormItem>
-          <FormItem>
-            <WhiteTextField
-              fullWidth
-              id="phone"
-              name="phone"
-              label="Phone"
-              type="tel"
-              value={formik.values.phone}
-              onChange={handlePhoneChange}
-              error={formik.touched.phone && Boolean(formik.errors.phone)}
-              helperText={formik.touched.phone && formik.errors.phone}
-            />
-          </FormItem>
-        </FormRow>
-        <SubmitButton
-          color="primary"
-          variant="contained"
-          fullWidth
-          type="submit"
-          value="Send"
-        >
-          Submit
-        </SubmitButton>
-      </form>
-    </div>
+    <Link href={formUrl} target="_blank" rel="noopener">
+      <SubmitButton variant="contained">REGISTER</SubmitButton>
+    </Link>
   );
 };
 
-export default WithMaterialUI;
+export default BasicButtons;
